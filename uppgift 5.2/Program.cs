facb9e87839db36848ab1ba18a368b0363859ba4
Meysam03 <simon.munter@icloud.com>
2023-01-20 10:37:40 +0100

    Add project files.

---
+++ uppgift 5.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; 
+
+namespace uppgift5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in hur många heltal du vill skriv in"); 
+            int index = int.Parse(Console.ReadLine());
+            int[] array = new int[index];
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Skriv in tal: " + i);
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+        }
+    }
+}
